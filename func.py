e519b9cfddcc13c98fe81ebafcc10e546a6ed872
Robotics-now <adityaraj.imac@gmail.com>
2025-12-18 18:56:49 -0500

    user arc

--- func.py
+++ func.py
@@ -1,16 +1,8 @@
-def show_numbered_list(todos):
-    ''' Print a nubered list of to-do items from a list '''
-    # new_todos = [item.strip('\n') for item in todos]. Could be also used to strip \n
-
-    for index, i in enumerate(todos):
-        i = i.strip('\n')
-        print(f"{index + 1}. {i.capitalize()}")
-
-def r_wFromFile(mode = "r", todos=[]):
+def r_wFromFile(file_name, mode = "r", todos=[]):
     '''Reads from or writes to ToDo.txt file based on mode parameter
     mode: "r" for read, "w" for write'''
 
-    with open("ToDo.txt", mode) as file_local:
+    with open(file_name, mode) as file_local:
         match mode:
             case "r":
                 todos = file_local.readlines()
--- web.py
+++ web.py
@@ -5,12 +5,50 @@ import json
 with open("data.json", "r") as json_file:
     data = json.load(json_file)
 
-todos = r_wFromFile()
+user = ''
+
+if "show_popup" not in st.session_state:
+    st.session_state.show_popup = False
+
+if "username" not in st.session_state:
+    st.title("Login")
+    user_input = st.text_input("Username", placeholder="Enter your username",).strip()
+    user = user_input.lower()
+    
+    st.session_state['username'] = user
+    st.stop()
+
+users = r_wFromFile(file_name="Users.txt", mode="r")
+
+    
+    
+if user in users:
+    st.success(f"Welcome back, {st.session_state['username']}!")
+    st.rerun()
+
+elif user not in users:
+    with st.container():
+        st.session_state.show_popup = True
+        st.write("You are not registered. Click below to sign up.")
+
+        st.button("Sign Up", on_click=lambda: st.session_state.update(show_popup=False))
+        
+        users = r_wFromFile(file_name="Users.txt", mode="r")
+        users.append(user + '\n')
+        r_wFromFile(file_name="Users.txt", mode="w", todos=users)
+
+        r_wFromFile(file_name="files/" + st.session_state['username'] + ".txt", mode="w", todos=["You signed up\n"])
+
+        st.success("Signed up successfully!")
+        
+        st.rerun()
+
+todos = r_wFromFile(file_name="files/" + st.session_state['username'] + ".txt", mode="r")
 
 def addwebtodo():
     todo = st.session_state['newtodo']
     todos.append(todo + '\n')
-    r_wFromFile("w", todos)
+    r_wFromFile(file_name="files/" + st.session_state['username'] + ".txt", mode="w", todos=todos)
 
 st.title(data['app_name'])
 st.subheader("Welcome to the Web Page")
@@ -20,7 +58,7 @@ for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
-        r_wFromFile("w", todos)
+        r_wFromFile(file_name="files/" + st.session_state['username'] + ".txt", mode="w", todos=todos)
         del st.session_state[todo]
         st.rerun()
        
